2cf7122a20bb17d4d07b2fd1fcbd8de9c6a23fad
Brandon Lee <brandonlee2543@hotmail.com>
2020-10-16 17:22:23 -0400

    Updates

--- precache-manifest.2f6743f943597504c34a7be1887acede.js
+++ precache-manifest.2f6743f943597504c34a7be1887acede.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5442ae3fda67f031633865062e576468",
+    "revision": "febace147e30561e164f11e9e7ebe5a9",
     "url": "/blee_IO/index.html"
   },
   {
-    "revision": "751003aab240becf12f7",
+    "revision": "e9db1095ee4559dd26d3",
     "url": "/blee_IO/static/css/main.93909cc4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blee_IO/static/js/2.8e6d4e5d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "751003aab240becf12f7",
-    "url": "/blee_IO/static/js/main.16ee64b7.chunk.js"
+    "revision": "e9db1095ee4559dd26d3",
+    "url": "/blee_IO/static/js/main.f6ad4fba.chunk.js"
   },
   {
     "revision": "820893c710d35eeff1d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blee_IO/precache-manifest.e53ee4c0572981fb9ef1920b015bf714.js"
+  "/blee_IO/precache-manifest.2f6743f943597504c34a7be1887acede.js"
 );
 
 self.addEventListener('message', (event) => {
